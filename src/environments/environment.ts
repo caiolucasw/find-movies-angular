2b5fdc2b82f4c57cf92775f846ee4a8fcddd9dde
Caio <caiolsds@gmail.com>
2024-01-07 13:11:07 -0300

    Some improvements

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://api.themoviedb.org',
+    apiKey: '06251c7d22907cef8991884cdcbaa078',
+    token: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNjI1MWM3ZDIyOTA3Y2VmODk5MTg4NGNkY2JhYTA3OCIsInN1YiI6IjYzOTUwNDI0OWY1ZGZiMDA3ZGY5MDMyZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Z4PgpktDb99_jPApXDVDv4WbwQv7NvdrpJsIh2_AZls'
+};
