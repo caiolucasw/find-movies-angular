4ef25048eb41b3b90bac7ac7b5a1faf0b8922188
Caio <caiolsds@gmail.com>
2024-01-08 16:04:48 -0300

    Method to calculare movie time was fixed, and images improvements

--- src/app/pipes/format-movie-time.pipe.ts
+++ src/app/pipes/format-movie-time.pipe.ts
@@ -6,10 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FormatMovieTimePipe implements PipeTransform {
 
   transform(value: number, ...args: unknown[]): unknown {
-    if ((value / 60) < 1) return `${value}m`
+    if ((value / 60) < 1) {
+      return value < 10 ? `0${value}m` : `${value}m`
+    } 
     
     const hours = Math.floor(value / 60);
-    const min = hours % 60;
+    const min = value % 60;
     const leadingZero = min < 10 ? '0' : '';
     return `${hours}h ${leadingZero}${min}m`;
   }
